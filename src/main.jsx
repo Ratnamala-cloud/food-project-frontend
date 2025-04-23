386131a6df89156902af36d16d480858330c1ccc
Ratnamala <gadwalaratnamala1515@gmail.com>
2025-04-23 08:50:01 -0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
@@ -15,13 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </BrowserRouter>
 
 
-=======
-import {BrowserRouter} from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-  <App />
-</BrowserRouter>
+
+
+
   
->>>>>>> 78b24c6 (first commit)
+
 )
+
